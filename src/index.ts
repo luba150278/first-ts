ba1901e8c27b99859acc3197e5f7ae8508bb5926
Lyubov Myetolkina <luba150278@gmail.com>
2022-12-02 11:47:02 +0200

    all

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,22 @@
 const world = 'world';
 
-function hello(who: string = world): string {
-  return `Hello ${who}! `;
+interface IProps {
+  who: string;
+  how: number;
+  func(a: number, b: number): number;
+}
+const func = (a: number, b: number): number => {
+  return a + b;
+};
+const props: IProps = {
+  who: 'world',
+  how: 3,
+  func,
+};
+
+function hello(props: IProps): string {
+  const res = props.func(6, 7);
+  return `Hello ${props.who}! ${props.how} times, result: ${res} `;
 }
 
-console.log(hello());
+console.log(hello(props));
